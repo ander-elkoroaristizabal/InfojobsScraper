5562c6453ee1e81882fe7865956a1c52a427c1e0
Ander Elkoroaristizabal <ander.elkoroaristizabal@gmail.com>
2021-03-28 22:24:22 +0200

    Csv output and execution info added.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from OnePageScraper import scrap_this_page
 from SearchPageScraper import scrape_search_results
 import pandas as pd
 from time import sleep
+from datetime import datetime
 
 
 def get_link_info(list_with_urls):
@@ -17,10 +18,8 @@ def get_link_info(list_with_urls):
     # df['url'] = url_list
     dict_list = []
     for url in list_with_urls:
-        sleep(2.5)
+        sleep(2)
         job_info = scrap_this_page(url)
-        print(url)
-        print(job_info)
         job_info['url'] = url
         dict_list.append(job_info)
     return pd.DataFrame(dict_list)
@@ -31,5 +30,11 @@ if __name__ == '__main__':
         "Data Scientist"
     print()
     url_list = scrape_search_results(keywords)
+    print()
+    print("Number of offers for analyzing:", len(url_list))
+    print()
     res = get_link_info(url_list)
+    now = datetime.now()
+    filename = keywords.replace(" ", "_") + "_" + now.strftime("%d-%m-%Y_%H_%M_%S") + ".csv"
+    res.to_csv("Examples/" + filename, index = False)
     print(res)
